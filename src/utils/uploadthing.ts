1e9c0d7253c28ac78267d3700d755c52f1140112
Jay Mawari <jaymawari31@gmail.com>
2025-07-11 13:19:17 +0530

    business id error

--- src/utils/uploadthing.ts
+++ src/utils/uploadthing.ts
@@ -3,5 +3,5 @@ import { generateUploadButton } from "@uploadthing/react";
 
 // Replace with your actual server URL
 export const UploadButton = generateUploadButton({
-  url: "http://localhost:3002/api/uploadthing", // or your deployed server
+  url: "https://message-booking.onrender.com/api/uploadthing", // or your deployed server
 });
