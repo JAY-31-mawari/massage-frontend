6ab6be4d7da149f7917528d400c31f5f85cfc5f9
Jay Mawari <jaymawari31@gmail.com>
2025-08-07 14:49:44 +0530

    provides steps progress sidebar

--- src/utils/uploadthing.ts
+++ src/utils/uploadthing.ts
@@ -2,5 +2,5 @@ import { generateUploadButton } from "@uploadthing/react";
 
 //Replace with your actual server URL
 export const UploadButton = generateUploadButton({
-  url: "http://localhost:3002/api/uploadthing", // or your deployed server
+  url: global.config.ROOTURL.prod + "/api/uploadthing", // or your deployed server
 });
